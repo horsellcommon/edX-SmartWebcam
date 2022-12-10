819c1cf510daefc8ab56f712fdad100dafecab94
horsellcommon <crawfordj716@gmail.com>
2022-12-10 11:23:54 +0000

    Boilerplate code

---
+++ script.js
@@ -0,0 +1,6 @@
+import * as tf from "@tensorflow/tfjs";
+
+const status = document.getElementById("status");
+if (status) {
+    status.innerText = "Loaded TensorFlow.js - version: " + tf.version.tfjs;
+}
