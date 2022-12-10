da10b613efd1b3ba2cd087d6c812f9ccfbd8dc38
horsellcommon <crawfordj716@gmail.com>
2022-12-10 11:52:49 +0000

    Grab the webcam

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const video = document.getElementById("webcam");
 const liveView = document.getElementById("liveView");
 const demosSection = document.getElementById("demos");
 const enableWebcamButton = document.getElementById("webcamButton");
+
+const getUserMediaSupported = () => {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+};
+
+if (getUserMediaSupported()) {
+  enableWebcamButton.addEventListener("click", enableCam);
+} else {
+  console.warn("getUserMedia() is not supported by your browser");
+}
